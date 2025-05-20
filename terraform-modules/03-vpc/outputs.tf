50bad7e63b4828b4c488a43929b15b65af233144
Mustafa Sharif <mohamedmu2025@gmail.com>
2025-05-20 20:32:02 +0200

    updating the output for rds output

--- terraform-modules/03-vpc/outputs.tf
+++ terraform-modules/03-vpc/outputs.tf
@@ -1,7 +1,14 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "private_subnet_ids" {
+  value = [for subnet in aws_subnet.private : subnet.id]
+}
+
 output "private_subnet_ids_with_names" {
-  description = "Map of subnet names to subnet IDs"
   value = {
-    for name, subnet in aws_subnet.private :
-    name => subnet.id
+    for subnet in aws_subnet.private :
+    subnet.tags["Name"] => subnet.id
   }
 }
