3403c78a784f5bbe316fc9bf0cf552a14938b9d9
Mustafa Sharif <mohamedmu2025@gmail.com>
2025-05-20 20:13:35 +0200

    updating the output for rds

--- terraform-modules/03-vpc/outputs.tf
+++ terraform-modules/03-vpc/outputs.tf
@@ -1,14 +1,7 @@
-output "vpc_id" {
-  value       = aws_vpc.main.id
-  description = "The ID of the VPC"
-}
-
-output "private_subnet_ids" {
-  value       = [for s in aws_subnet.private : s.id]
-  description = "IDs of private subnets"
-}
-
-output "private_subnet_azs" {
-  value       = [for s in aws_subnet.private : s.availability_zone]
-  description = "Availability zones of private subnets"
+output "private_subnet_ids_with_names" {
+  description = "Map of subnet names to subnet IDs"
+  value = {
+    for name, subnet in aws_subnet.private :
+    name => subnet.id
+  }
 }
