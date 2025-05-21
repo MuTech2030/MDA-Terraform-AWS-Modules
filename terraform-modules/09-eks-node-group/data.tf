670ea7c6064165b073b101a165604639f7757832
Mustafa Sharif <mohamedmu2025@gmail.com>
2025-05-21 23:00:30 +0200

    update eks node group output.tf

--- terraform-modules/09-eks-node-group/data.tf
+++ terraform-modules/09-eks-node-group/data.tf
@@ -9,20 +9,6 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-
 output "node_group_name" {
   value = aws_eks_node_group.this.node_group_name
 }
--- terraform-modules/09-eks-node-group/outputs.tf
+++ terraform-modules/09-eks-node-group/outputs.tf
@@ -3,15 +3,7 @@ output "node_role_arn" {
   description = "ARN of the IAM role for node group"
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
+output "node_group_name" {
+  value = aws_eks_node_group.this.node_group_name
 }
