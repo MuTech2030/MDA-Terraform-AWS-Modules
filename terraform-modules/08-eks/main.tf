62462744d0b9a4a5048a71a8f2415344559c49cb
Mustafa Sharif <mohamedmu2025@gmail.com>
2025-05-21 18:33:24 +0200

    adding the eks policy and attach it to the role

--- terraform-modules/08-eks/main.tf
+++ terraform-modules/08-eks/main.tf
@@ -4,13 +4,23 @@
 # modules/eks_iam_role/main.tf
 resource "aws_iam_role" "eks_cluster" {
   name = var.name
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
 }
 
-
-
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_cluster.name
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
 }
+
+
+
 
